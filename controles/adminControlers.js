1d282f39b8efbc58175e8e17df391b19c34570a6
AdityanBS <adithadith02@gmail.com>
2022-08-01 20:52:26 +0530

    only 2 admins are allowed now and next it has to be optimized

--- controles/adminControlers.js
+++ controles/adminControlers.js
@@ -1,14 +1,22 @@
 const adminModel=require('../models/adminModel')
 const passport=require('passport')
+let adminCount
 
 exports.adminLogin=(req,res)=>{
-    res.render('admin/adminLogin')
+    adminModel.find().exec(function(err,result){
+        let Count=result.length
+        if(Count>2){
+            adminCount=true
+        }
+        console.log(adminCount+">>>>>>>>>>>>>>>");
+    })
+    res.render('admin/adminLogin',{adminCount:adminCount})
 }
 exports.adminSignup=(req,res)=>{
     res.render('admin/adminSignup')
 }
 exports.adminSignupData=(req,res)=>{
-    // console.log(req.body);
+    console.log(req.body);
     let adminSignup= new adminModel({
         email:req.body.email,
         password:req.body.password
@@ -18,6 +26,8 @@ exports.adminSignupData=(req,res)=>{
     }).catch((error)=>{
         console.log(error);
     })
+
+    
     res.redirect('adminLogin')
 }
 exports.adminHome=(req,res)=>{
