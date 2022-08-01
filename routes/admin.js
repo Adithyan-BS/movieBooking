f027f92607fbf7fbc6d72504d2603044a65e393f
AdityanBS <adithadith02@gmail.com>
2022-08-01 10:27:04 +0530

    admin login page and aadmin signup page is rendering and data from frontend is now receving in server and next step it should be validated and insrted and insterted to data base in a new collection

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,13 @@
 const express=require('express');
 const router=express.Router();
+router.get('/adminLogin',(req,res)=>{
+    res.render('admin/adminLogin')
+})
+router.get('/AdminCreateAccount',(req,res)=>{
+    res.render('admin/adminSignup')
+})
+router.post('/AdminSignupData',(req,res)=>{
+    console.log(req.body);
+})
 
 module.exports=router
