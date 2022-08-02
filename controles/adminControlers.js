ad6c391f74be7d98ab735e0867d2be84c8c7fea8
AdityanBS <adithadith02@gmail.com>
2022-08-02 14:12:31 +0530

    only one admin can be created and if a someone try to create a new admin it will show error and redirect to adminLogin

--- controles/adminControlers.js
+++ controles/adminControlers.js
@@ -1,34 +1,50 @@
 const adminModel=require('../models/adminModel')
 const passport=require('passport')
+const flash=require('express-flash')
 let adminCount
 
 exports.adminLogin=(req,res)=>{
     adminModel.find().exec(function(err,result){
-        let Count=result.length
-        if(Count>2){
-            adminCount=true
-        }
-        console.log(adminCount+">>>>>>>>>>>>>>>");
+         adminCount=result.length === 0 ? true : false;        
+        res.render('admin/adminLogin',{adminCount:adminCount})  
     })
-    res.render('admin/adminLogin',{adminCount:adminCount})
-}
-exports.adminSignup=(req,res)=>{
-    res.render('admin/adminSignup')
-}
-exports.adminSignupData=(req,res)=>{
-    console.log(req.body);
-    let adminSignup= new adminModel({
-        email:req.body.email,
-        password:req.body.password
-    })
-    adminSignup.save().then((data)=>{
-        console.log(data);
-    }).catch((error)=>{
-        console.log(error);
-    })
-
     
-    res.redirect('adminLogin')
+}
+exports.adminSignup=(req,res)=>{    
+    res.render('admin/adminSignup')
+    
+}
+exports.adminSignupData=async (req,res)=>{
+
+  let preventSignup= await adminModel.find()
+  console.log(preventSignup);
+  
+    if(preventSignup.length == 0){
+        console.log('creating admin account');
+        let adminSignup= new adminModel({
+            email:req.body.email,
+            password:req.body.password
+        })
+        adminSignup.save().then((data)=>{
+            console.log(data);
+            res.redirect('adminLogin')
+        }).catch((error)=>{
+            console.log(error);
+        })
+        
+    }else{
+       
+        console.log('admin found');
+
+        console.log('trying to send req.flash message');
+        req.flash('info',"Admin allready found, cannot create a new admin... You will be redirect to login page.")
+        setTimeout(() => {
+            res.redirect('adminLogin')
+        }, 3000);
+
+    }
+    
+
 }
 exports.adminHome=(req,res)=>{
     res.render('admin/adminHome')
